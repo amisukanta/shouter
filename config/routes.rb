405f25235f87836d38b6011818dfa19bdac628e1
amisukanta <amisukanta02@gmail.com>
2015-04-26 13:26:33 +0200

    Shouter added for authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#show', via: :get
+  resource :dashboard, only: [:show]
+  resources :shouts, only:[:create, :show]
   devise_for :users
+  resources :users, only: [:show]
 end
